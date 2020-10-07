96eaed58caf4cc1539b962538a0862a9e72efe8c
Matthew Finch <matt.fiinch92@gmail.com>
2020-10-07 02:25:56 -0400

    20201007

--- Solutions/findMedianSortedArray.py
+++ Solutions/findMedianSortedArray.py
@@ -1,6 +1,16 @@
 class Solution:
-    def findMedianSortedArrays(self, nums1, nums2):
-        array = sorted(nums1 + nums2)
-        if len(array) % 2 == 0: return (array[len(array) // 2] + array[len(array) // 2 + 1]) / 2
-        else: return array[len(array) // 2]
-
+    
+        def findMedianSortedArrays(self, arr_1, arr_2):
+            # set the array
+            array = sorted(arr_1 + arr_2)
+            print(f'Combined Array, Sorted: \n {array} \n ')
+            print('Is array length even or odd? \n')
+             # if the array's length is even
+            if len(array) % 2 == 0: 
+                print(f'Even -- Number of Elements: {len(array)}')
+                return (array[len(array) // 2] + array[len(array) // 2 + 1]) / 2
+        
+            # if the array's length is odd
+            else: 
+                print(f'Odd -- Number of Elements: {len(array)} \n')
+                return array[len(array) // 2]
--- Solutions/lengthOfLongestSubString.py
+++ Solutions/lengthOfLongestSubString.py
@@ -1,8 +1,31 @@
 class Solution:
-    def lengtOfLongestSubstring(self, s):
-        max_, start, chars = 0, 0, {}
+
+    def lengthOfLongestSubstring(self, s):
+
+        substring_len = 0 
+        count = 0
+        chars_counter = {}
+
+        print(f'Substring Length: {substring_len}', end='\n * ')
+        print(f'Count: {count}', end='\n * ')
+        print('[+] Character counter Set')
+        print('\n')
+
         for idx in range(len(s)):
-            if s[idx] in chars and start <= chars[s[idx]]: start = chars[s[idx]] + 1
-            else: max_ = max(max_, idx - start + 1)
-            chars[s[idx]] = idx
-        return max_
+            print(f' [i] currently at index: {idx}')
+            if s[idx] in chars_counter and count <= chars_counter[s[idx]]: 
+                print(f'[---] Current Element: {s[idx]}')
+                print(f'[---] Count at: {count}')
+                print('\n')
+                count = chars_counter[s[idx]] + 1
+                print('[i] Charater Counted \n')
+            else: 
+                print(f'[---] Current Element: {s[idx]}')
+                print(f'[---] Substring Length at: {substring_len}')
+                print('\n')
+                substring_len = max(substring_len, idx - count + 1)
+            chars_counter[s[idx]] = idx
+            print(f'Current Counts: {chars_counter}')
+            print('\n')
+
+        return substring_len
