41ef8ba2d1f06218494c9367263601ce5e499596
Matthew Finch <matt.fiinch92@gmail.com>
2020-10-08 23:43:24 -0400

    20201008

---
+++ Solutions/containerWithMostWater.py
@@ -0,0 +1,25 @@
+class Solution:
+
+    def maxArea(self, height):
+
+        # start with no area
+        max_area = 0 
+        # set the moving point at zero
+        idx = 0
+        # set threshold at which line is slanted
+        jdx = len(height) - 1
+
+        # train moving point to meet point of max area
+        while idx < jdx:
+            # define max areals
+            max_area = max(max_area, min(height[idx], height[jdx]) * (jdx -  idx))
+            print(f'Current Max Area: {max_area}\n')
+            # set unit of learning by condition of line
+            if height[idx] < height[jdx]:
+                idx += 1
+            else:
+                jdx -= 1
+
+        print(f'Final Max Area: {max_area}\n')
+
+        return max_area
---
+++ Solutions/intoRomanNumerial.py
@@ -0,0 +1,13 @@
+class Solution:
+
+    def intToRoman(self, n: int) -> str:
+
+        r_num = "M" * (n // 1000)
+        r_num += "CM" if n % 1000 >= 900 else "D" *((n % 1000) // 500)
+        r_num += "CD" if n % 500 >= 400 and r_num[-2:] != "CM" else "C" * ((n % 500) // 100)  if n % 500 < 400 else ""
+        r_num += "XC" if n % 100 >= 90 else "L" * ((n % 100) // 50)
+        r_num += "XL" if n % 50 >= 40 and r_num[-2:] != "XC" else "X" * ((n % 50) // 10)  if n % 50 < 40 else ""
+        r_num += "IX" if n % 10 >= 9 else "V" * ((n % 10) // 5)
+        r_num += "IV" if n % 5 >= 4 and r_num[-2:] != "IX" else "I" * ((n % 5) // 1) if n % 5 < 4 else ""
+        
+        return r_num
