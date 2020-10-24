ef7418a7fa341efa5de86b021c2bad5894cc0e04
Matthew Finch <matt.fiinch92@gmail.com>
2020-10-24 14:15:27 -0400

    201024

---
+++ Solutions/letterCombinations.py
@@ -0,0 +1,36 @@
+
+
+class Solution:
+
+    def letterCombinations(self, digits):
+
+        keypad_dic = {
+
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz'
+        }
+
+        result = ['']
+
+        for digit in digits:
+            temp = []
+            for y in result:
+                for x in keypad_dic[digit]:
+                    temp.append(y + x)
+                result = temp
+
+        return result if any(result) else []
+
+
+
+if __name__ == '__main__':
+
+    digits = '23'
+    Solution().letterCombinations(digits)
+
