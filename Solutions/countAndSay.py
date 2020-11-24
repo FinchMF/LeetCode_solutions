4c35dd5cef908d9e3991e985a299d385b25f0761
Matthew Finch <finchrmatthew@gmail.com>
2020-11-24 18:11:40 -0500

    countandsay

---
+++ Solutions/countAndSay.py
@@ -0,0 +1,32 @@
+
+class Solution:
+
+    def countAndSay(self, n):
+
+        curr = '1'
+
+        for i in range(n-1):
+
+            tmp, cnt = "", 1
+
+            for j, c in enumerate(curr):
+
+                if j > 0 and curr[j - 1] == c:
+                    cnt += 1
+
+                elif j > 0:
+                    tmp += str(cnt) + curr[j - 1]
+                    cnt = 1
+                
+                if j == len(curr) - 1:
+                    tmp += str(cnt) + curr[j]
+
+            curr = temp
+
+        return curr
+
+
+if __name__ == '__main__':
+
+    n = 4
+    Solution().countAndSay(n)
