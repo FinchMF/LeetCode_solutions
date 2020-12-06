71ee6288264c7fe8e740f6fc55c25da8f3bd5e2f
Matthew Finch <finchrmatthew@gmail.com>
2020-12-06 13:02:25 -0500

    pow

---
+++ Solutions/pow.py
@@ -0,0 +1,15 @@
+
+
+class Solution:
+
+    def myPow(self, x: float, n: int) -> float:
+        
+        return float(x**n)
+
+
+if __name__ == '__main__':
+
+    x, n = 2.00000, 10
+    Solution().myPow(x, n)
+
+    
