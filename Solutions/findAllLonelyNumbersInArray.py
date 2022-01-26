00d25aa53353b232a6e11c8e8e9ea00985a2cb1f
Matthew Finch <finchrmatthew@gmail.com>
2022-01-25 19:38:07 -0500

    edited

--- Solutions/findAllLonelyNumbersInArray.py
+++ Solutions/findAllLonelyNumbersInArray.py
@@ -1,44 +1,11 @@
-
+import collections
 
 class Solution(object):
 
     def findLonely(self, nums: list) -> list:
 
-        counter = {}
-
-        for n in nums:
-
-            if n in counter:
-
-                counter[n]['number'] += 1
-            else:
-                counter[n] = {
-
-                    'number': 1, 
-                    'neighbors': False
-                }
-
-            neighbor_left = (n - 1) in nums
-            neighbor_right = (n + 1) in nums
-
-            if neighbor_left or neighbor_right:
-
-                counter[n]['neighbors'] = True
-
-
-        lonely = []
-
-        for n, details in counter.items():
-
-            if details['number'] == 1:
-                lonely.append(n)
-
-            if details['neighbors'] == False:
-                lonely.append(n)
-
-
-        return lonely
-
+        counter = collections.Counter(nums)
+        return [n for n in nums if counter[n] == 1 and n-1 not in counter and n+1 not in counter]
 
 if __name__ == '__main__':
 
